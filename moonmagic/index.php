79d538315d400eb60bfd364c75ca8d83789dfdb6
htmldiz <mark.tanasevich@gmail.com>
2020-03-23 16:49:11 +0200

    update

--- moonmagic/index.php
+++ moonmagic/index.php
@@ -44,36 +44,24 @@ foreach ($filedata as $data) {
 	$arrayout[$index][] = $data;
 }
 $out_array_js = array();
-// foreach ($arrayout as $cnt => $array) {
-// 	foreach ($array as $key => $dt) {
-// 		$to           = $dt['to'];
-// 		preg_match('/https:\/\/www.dropbox.com\/.*&preview=/', $to, $output_array);
-// 		if(!empty($output_array)){
-// 			$to = str_replace($output_array[0], '/'.$counter.'/', $to);
-// 			$dt['to'] = $to;
-// 		}
-// 		// $arrayout[$cnt][$key] = $dt;
-// 		$arrayout[$cnt][$key] = $dt;
-// 	}
-// 	$counter++;
-// }
-ob_start();
-echo '<body>';
-foreach ($arrayout as $key => $value) {
-	echo '<div>';
-	echo $key.'<br>';
-	echo $counter.'<br>';
-	foreach ($value as $val) {
-		echo '<a href="'.$val['to'].'" target="_blank">'.$val['to'].'</a>'.'<br>';
+foreach ($arrayout as $cnt => $array) {
+	foreach ($array as $key => $dt) {
+		$to           = $dt['to'];
+		preg_match('/https:\/\/www.dropbox.com\/.*&preview=/', $to, $output_array);
+		if(!empty($output_array)){
+			$to = str_replace($output_array[0], '/'.$counter.'/', $to);
+			$dt['to'] = $to;
+		}
+		// $arrayout[$cnt][$key] = $dt;
+		$arrayout[$cnt][$key] = $dt;
 	}
-	echo '</div>'.'<br>';
 	$counter++;
 }
-echo '</body>';
-// var_dump($arrayout);
+ob_start();
+echo json_encode($arrayout);
 $file_content = ob_get_contents();
 ob_end_clean();
-$file = fopen('outscaner.html', 'w+');
+$file = fopen('outscaner.json', 'w+');
 fwrite($file, $file_content);
 fclose($file);
 ?>
