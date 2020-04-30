b8b28a970826bae5297005b17399b8197428b9fe
oliinykroman <oliinykroman@gmail.com>
2020-04-30 16:53:10 +0300

    update

--- skylum/src/js/script.js
+++ skylum/src/js/script.js
@@ -1,8 +1,11 @@
 $('.header .header__wrap').html ('<h2 class="sk-h2">Thank you for the purchase! Download link is sent to your e-mail</h2>');
 
-$('.after-head__figure.preload').appendTo('.tone-mapping');
+
 
 $('.tone-mapping__slider-wrap').prependTo('.after-head__head .container');
+
+$('.after-head__figure.preload').appendTo('.tone-mapping.sk-section');
+
 $('.tone-mapping__slider-wrap').prepend('<div class="slide-text-holder"><h3 class="slide-title">It’s hard to capture a perfect light sometimes, right?</h3><p>Create a perfect image from a few too dark or too bright photos with high dynamic range editing (HDR) in a few clicks!</p></div>')
 
 $('.after-head__platforms.sk-text_type_neutral').after('<div class="text-holder_platforms"><span class="txt-digit txt-line-through">99$</span> only <span class="txt-digit-bg">69$</span> together with Luminar 4</div>');
