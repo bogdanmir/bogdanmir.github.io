9ee54c366df09ea4f0323ee84b053894d3c282b9
oliinykroman <oliinykroman@gmail.com>
2020-05-07 15:19:47 +0300

    skylum scroll

--- skylum_landing/src/js/script.js
+++ skylum_landing/src/js/script.js
@@ -1,4 +1,7 @@
 $('body').find('.common-hero .info-label-wr').detach().insertAfter( ".header .header__wrap .header__menu" );
 
-$('body').find('.common-hero .row:nth-child(1) > div').removeClass('col-md-8 offset-md-2').addClass('col-md-6');
+// $('body').find('.common-hero .row:nth-child(1) > div').removeClass('col-md-8 offset-md-2').addClass('col-md-6');
+$('body').find('.common-hero .common-hero__content-wrap').detach().prependTo('.common-hero .row:nth-child(2) > div');
 $('body').find('.common-hero .row:nth-child(2) > div').addClass('col-md-6');
+
+$('body').find('.common-hero .row:nth-child(2)').append('<div class="col-md-6"><p class="common-hero__content-text">Standalone application for Mac & PC with full RAW support. And Plugin for Adobe Photoshop, Lightroom & Apple products</p></div>');
