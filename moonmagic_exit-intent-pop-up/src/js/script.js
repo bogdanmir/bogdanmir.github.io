f100cc5fca534a506e8132d8b9793af67e23f81d
Mark Tanasevich <marktanasevich@MacBook-Air-Mark.local>
2020-06-06 17:03:16 +0300

    update

--- moonmagic_exit-intent-pop-up/src/js/script.js
+++ moonmagic_exit-intent-pop-up/src/js/script.js
@@ -4,7 +4,6 @@ if( $('.cart-popup .cart-popup__list .cart-popup__item').length > 1 ){
 } else {
     var item_content = "We can’t guarantee its availability<br> if you don't complete the purchase now";
 }
-
 window.slide_items = new Array();
 $('.cart-popup .cart-popup__list .cart-popup__item').each(function() {
     var item_img = $(this).find('.cart-popup__item-image').html();
@@ -75,6 +74,40 @@ if( $('.cart-popup .cart-popup__list .cart-popup__item').length > 1 ){
     });
 } else {
     console.log('one item in card')
+}
+window.already_display_popup = 0;
+window.show_popup = function(){
+    if(window.already_display_popup === 0){
+        $('.exit_popup_container').addClass('active');
+        $('.exit_popup_overlay').addClass('active');
+    }
+    window.already_display_popup = 1;
+}
+var old_scroll,scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+$(window).scroll(function(e){
+    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+});
+setInterval(function() {
+    if(old_scroll-50 > scrollTop){
+        if(typeof window.show_popup != 'undefined'){
+            window.show_popup();
+        }
+    }
+    old_scroll = scrollTop;
+});
+var regex = /moonmagic\.com/g;
+var str = document.referrer;
+var is_need_block_history = regex.exec(str) == null;
+window.onpopstate = function(event) {
+    if(is_need_block_history && window.already_display_popup === 0){
+        var state = event.state;
+        window.show_popup();
+        history.pushState({page: 4}, "Wait a minute", "?test=true");
+    }
+}
+if(is_need_block_history){
+    history.pushState({page: 1}, "Wait a minute", "?test=true");
+    history.pushState({page: 2}, "Wait a minute", "?test=true");
 }
 $('body').find('.swiper-slide').prepend("<div class='slider_tooltip_box'>This is a popular choice,<br> we may run out of stock soon</div>");
 
