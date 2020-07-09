36b17f1a7a1fa819dde18504db9079fc6712d91c
oliinykroman <oliinykroman@gmail.com>
2020-07-10 01:10:51 +0300

    update

--- depositphotos_ time-delay-popup/src/js/script.js
+++ depositphotos_ time-delay-popup/src/js/script.js
@@ -162,10 +162,13 @@ $(document).on('click','.modal_time_delay .modal-overlay', function(){
 });
 
 
-// chekout
-if( localStorage.getItem('modal_show') == 'true' ) {
-	$('.order-info_trial .order-info__items').html('<div class="order-info__item"><i class="icon icon-ok-dark"></i><span class="order-info__item-text">Download any 10 images or vectors for free</span></div><div class="order-info__item"><i class="icon icon-ok-dark"></i><span class="order-info__item-text">7 days free, then $299 annualy for 360 images</span></div><div class="order-info__item"><i class="icon icon-ok-dark"></i><span class="order-info__item-text">Cancel anytime before your free trial is over</span></div><div class="order-info__item"><i class="icon icon-ok-dark"></i><span class="order-info__item-text">Unused downloads transfer to the next month</span></div>')
-}
+// checkout
+window.interval_out2 = setInterval(function(){
+	if( localStorage.getItem('modal_show') == 'true' ) {
+		$('.order-info_trial .order-info__items').html('<div class="order-info__item"><i class="icon icon-ok-dark"></i><span class="order-info__item-text">Download any 10 images or vectors for free</span></div><div class="order-info__item"><i class="icon icon-ok-dark"></i><span class="order-info__item-text">7 days free, then $299 annualy for 360 images</span></div><div class="order-info__item"><i class="icon icon-ok-dark"></i><span class="order-info__item-text">Cancel anytime before your free trial is over</span></div><div class="order-info__item"><i class="icon icon-ok-dark"></i><span class="order-info__item-text">Unused downloads transfer to the next month</span></div>')
+	}
+},500);
+
 jQuery(document).ready(function($) {
 	setTimeout(function(){
 		// setInterval(function(){
