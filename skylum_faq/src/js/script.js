eba93f40239d72e00e72517682f72bd5b470c53c
oliinykroman <oliinykroman@gmail.com>
2020-07-16 18:48:44 +0300

    skylum faq

---
+++ skylum_faq/src/js/script.js
@@ -0,0 +1,196 @@
+// language.some_slug
+// alert(language.some_slug);
+
+$outhtml  = '<section class="faq-section">'
+$outhtml += 	'<div class="container">'
+$outhtml += 		'<div class="row">'
+
+$outhtml += 			'<div class="col-12"><h2 class="faq-title">Frequently asked questions</h2></div>'
+
+$outhtml +=				'<div class="col-12 col-md-6 col-lg-6 col-xl-6">'
+$outhtml +=					'<ul class="questions-list">'
+$outhtml +=						'<li class="question-item">'
+$outhtml += 						'<div class="question-item_title">What should I do if I have any payment issues? <span class="arrow-ico"></span></div>'
+$outhtml +=							'<div class="question_desc">'
+$outhtml +=								'<p>If you need help in resolving any payment issues, please contact our payment processing partner FastSpring <a href="https://fastspring.com/question-a-charge/">via this form</a> </p>'
+$outhtml +=							'</div>'
+$outhtml +=						'</li>'
+$outhtml +=						'<li class="question-item">'
+$outhtml += 						'<div class="question-item_title">How to install the application? <span class="arrow-ico"></span></div>'
+$outhtml +=							'<div class="question_desc">'
+$outhtml +=								'<p>Installation is simple and intuitive. In case you’ll have any issues with installation after purchase, please check out <b>installation guide for <a href="https://manual.skylum.com/4/en/topic/installing-activating-luminar-for-macos">Mac</a> or for <a href="https://manual.skylum.com/4/en/topic/installing-activating-luminar-for-win">Windows</a></b> and you can always contact our Support team via this <a href="https://skylum.com/support">form.</a></p>'
+$outhtml +=							'</div>'
+$outhtml +=						'</li>'
+
+$outhtml +=						'<li class="question-item">'
+$outhtml += 						'<div class="question-item_title">Will it work on my computer? What are the system requirements? <span class="arrow-ico"></span></div>'
+$outhtml +=							'<div class="question_desc">'
+$outhtml +=								'<p>Here is the list of minimum system requirements for Microsoft® Windows®:</p><p><b>Processor</b>	Intel® Core™ i5 generation or better</p><p><b>Graphics</b>	Open GL 3.3 or later compatible</p><p><b>Memory</b>	8 GB of RAM</p><p><b>System</b>	Windows 7® (only 64-bit OS), Windows 8.1® (only 64-bit OS), Windows 10® (only 64-bit OS)</p><p><b>Disk space</b>	10 GB free space</p><p><b>Monitor</b>	At least 1280×768 resolution</p>   <br>  <p>Here is the list of minimum system requirements for Apple® macOS®:</p>  <p><b>Processor</b>    Intel® Core™ i5 generation or better</p>   <p><b>Graphics</b>	Integrated graphics card</p>   <p><b>Memory</b>	8 GB of RAM</p>   <p><b>System</b>	macOS 10.12 or higher</p>   <p><b>Disk space</b> 10 GB free space</p>   <p><b>Monitor</b> At least 1280×768 resolution</p>'
+$outhtml +=							'</div>'
+$outhtml +=						'</li>'
+
+$outhtml +=						'<li class="question-item">'
+$outhtml += 						'<div class="question-item_title">How to get started? Do you have any tutorials or step-by-step guides? <span class="arrow-ico"></span></div>'
+$outhtml +=							'<div class="question_desc">'
+$outhtml +=								'<p>We have 80+ tutorials and beginners’ guides at <a href="https://skylum.com/academy">Skylum Academy.</a></p>'
+$outhtml +=							'</div>'
+$outhtml +=						'</li>'
+
+$outhtml +=						'<li class="question-item">'
+$outhtml += 						'<div class="question-item_title">Where can I see videos on how Luminar 4 works? <span class="arrow-ico"></span></div>'
+$outhtml +=							'<div class="question_desc">'
+$outhtml +=								'<p>The video tutorials are available <a href="https://skylum.com/video-galleries/luminar-video-tutorials?page=2&per-page=100">here</a>. We also invite you to check out our official <a href="https://www.youtube.com/user/macphunsoftware/playlists">YouTube channel</a> which contains video tutorials for Luminar 4.</p>'
+$outhtml +=							'</div>'
+$outhtml +=						'</li>'
+
+$outhtml +=						'<li class="question-item">'
+$outhtml += 						'<div class="question-item_title">Do I need to have any photo editing skills to use Luminar 4? <span class="arrow-ico"></span></div>'
+$outhtml +=							'<div class="question_desc">'
+$outhtml +=								'<p>Luminar is suitable for both beginners and pro photographers. You can either apply Luminar Looks to your photos (sets of adjustments created by professionals), or you can experiment with hundreds of tools and sliders by yourself.</p><p>With RAW files support, export to major file formats, layers, masks and much more,</p><p>Luminar suits all your photography needs.</p>'
+$outhtml +=							'</div>'
+$outhtml +=						'</li>'
+
+$outhtml +=						'<li class="question-item">'
+$outhtml += 						'<div class="question-item_title">How do AI features work? Is it hard to understand how to use them? <span class="arrow-ico"></span></div>'
+$outhtml +=							'<div class="question_desc">'
+$outhtml +=								'<p>The algorithms that power AI tools in Luminar 4 are complex and intelligent. Still, they are super-easy to use: Luminar allows you to quickly achieve real results that still remain under your control.</p>'
+$outhtml +=							'</div>'
+$outhtml +=						'</li>'
+
+$outhtml +=						'<li class="question-item">'
+$outhtml += 						'<div class="question-item_title">How to get support and answers to my questions? <span class="arrow-ico"></span></div>'
+$outhtml +=							'<div class="question_desc">'
+$outhtml +=								'<p>You can contact our support team via this <a href="https://skylum.com/support">form</a> and they’ll be glad to reply within 24 hours.</p>'
+$outhtml +=							'</div>'
+$outhtml +=						'</li>'
+
+$outhtml +=						'<li class="question-item">'
+$outhtml += 						'<div class="question-item_title">What are the terms of the payment? Is it a subscription? <span class="arrow-ico"></span></div>'
+$outhtml +=							'<div class="question_desc">'
+$outhtml +=								'<p>Unlike subscription plans, we are offering lifetime licenses; therefore, you will be able to use the application you‘ve purchased for an unlimited period of time.</p>'
+$outhtml +=							'</div>'
+$outhtml +=						'</li>'
+
+$outhtml +=						'<li class="question-item">'
+$outhtml += 						'<div class="question-item_title">Do I need to pay for updates? <span class="arrow-ico"></span></div>'
+$outhtml +=							'<div class="question_desc">'
+$outhtml +=								'<p>Each update to the current version of the Software will be distributed free of charge to you; you may be notified that the Update is available through an in-program notification, via the e-mail address you provided upon registration of the Software, or through our website.</p><p>Upgrades will be offered for a separate fee (we provide huge discounts for the owners of the older versions of our products). “Upgrade” means a new version of the Software issued by Skylum from time to time in its sole discretion (typically once every calendar year).</p>'
+$outhtml +=							'</div>'
+$outhtml +=						'</li>'
+
+$outhtml +=						'<li class="question-item">'
+$outhtml += 						'<div class="question-item_title">Does Luminar4 come with everything in it? Is it fully functional without Luminar Looks, Sky Packs and LUTs that are offered in addition to the license? <span class="arrow-ico"></span></div>'
+$outhtml +=							'<div class="question_desc">'
+$outhtml +=								'<p>Sure, Luminar is fully functional as is. Luminar comes with a variety of embedded Looks, LUTs, Sky Samples, Objects, etc. Purchasing additional items is completely optional.</p>'
+$outhtml +=							'</div>'
+$outhtml +=						'</li>'
+
+$outhtml +=					'</ul>'
+$outhtml +=				'</div>'
+
+
+$outhtml +=				'<div class="col-12 col-md-6 col-lg-6 col-xl-6">'
+$outhtml +=					'<ul class="questions-list">'
+$outhtml +=						'<li class="question-item">'
+$outhtml += 						'<div class="question-item_title">What is Aurora HDR? When HDR photo editing is needed? <span class="arrow-ico"></span></div>'
+$outhtml +=							'<div class="question_desc">'
+$outhtml +=								'<p>High Dynamic Range (HDR) photography is a popular photographic technique designed to overcome the limitations of image sensors in digital photographs. This process utilizes multiple exposures of the same subject, each shot at different exposure values (“brackets”). Those brackets are then automatically merged into a single shot which encompasses a greater exposure range.</p><p>Aurora HDR is a joint project between Skylum and Trey Ratcliff to produce the most powerful, simple and fast HDR photo editing software in the world for Mac and PC. Aurora HDR contains every imaginable tool needed to produce high-quality “next generation” dramatic images.</p>'
+$outhtml +=							'</div>'
+$outhtml +=						'</li>'
+
+$outhtml +=						'<li class="question-item">'
+$outhtml += 						'<div class="question-item_title">How does 30-Day Money-Back Guarantee work? <span class="arrow-ico"></span></div>'
+$outhtml +=							'<div class="question_desc">'
+$outhtml +=								'<p>If you are not completely satisfied with the Software for any reason, you may return the Software, together with your receipt or any other proof of purchase of the Software, for a refund of the money you paid for the Software (less any applicable taxes or other fees) at any time during the thirty (30) day period following the date of purchase. Upon receiving a refund you shall cease any and all use and destroy all copies, full or partial, of the Software for which you no longer possess a paid-up license. All refund requests are processed by our Support team, you can contact them via this <a href="https://skylum.com/support">form.</a></p>'
+$outhtml +=							'</div>'
+$outhtml +=						'</li>'
+
+$outhtml +=						'<li class="question-item">'
+$outhtml += 						'<div class="question-item_title">Do I need to have Photoshop or Lightroom installed to use Luminar 4? <span class="arrow-ico"></span></div>'
+$outhtml +=							'<div class="question_desc">'
+$outhtml +=								'<p>Luminar 4 works both in a standalone mode and as a plugin. Photoshop or Lightroom are not required for the app to work. Still, you may install Luminar as a plugin to these applications during the initial setup.</p>'
+$outhtml +=							'</div>'
+$outhtml +=						'</li>'
+
+$outhtml +=						'<li class="question-item">'
+$outhtml += 						'<div class="question-item_title">How to use Luminar 4 as a plugin for Photoshop, Lightroom, Photos for Mac? <span class="arrow-ico"></span></div>'
+$outhtml +=							'<div class="question_desc">'
+$outhtml +=								'<p>Please check out <a href="https://manual.skylum.com/4/en/topic/using-luminar-with-other-software">this tutorial</a> on how to use Luminar with other software.</p>'
+$outhtml +=							'</div>'
+$outhtml +=						'</li>'
+
+$outhtml +=						'<li class="question-item">'
+$outhtml += 						'<div class="question-item_title">What versions of Photoshop, Lightroom, Photos for Mac are supported? <span class="arrow-ico"></span></div>'
+$outhtml +=							'<div class="question_desc">'
+$outhtml +=								'<p>On Mac OS you can use Luminar 4 as a plugin for Lightroom (starting from version 4), Photoshop (from version CS5), Photoshop Elements (from version 13), Apple Photos, and Aperture.</p><p>On Windows you can use Luminar 4 as a plugin for Lightroom (starting from version 4), Photoshop Elements (from version 13) and Photoshop (from version CS5).</p>'
+$outhtml +=							'</div>'
+$outhtml +=						'</li>'
+
+$outhtml +=						'<li class="question-item">'
+$outhtml += 						'<div class="question-item_title">How to use Luminar 4 on multiple computers? <span class="arrow-ico"></span></div>'
+$outhtml +=							'<div class="question_desc">'
+$outhtml +=								'<p>Luminar 4 is meant to be used by a single person, that is why we offer a license for 1 device by default. But we realize that sometimes a person may have more than just one device that is why you have an option to change license quantity when selecting the items you would like to purchase.</p><p>Alternatively, you also have the ability to purchase an additional seat to your license later in your Skylum account. If you want to transfer your license to a new computer, you can always reset your license counter in your Skylum account.</p>'
+$outhtml +=							'</div>'
+$outhtml +=						'</li>'
+
+$outhtml +=						'<li class="question-item">'
+$outhtml += 						'<div class="question-item_title">Can I use the application in a standalone mode and as a plugin with one license? <span class="arrow-ico"></span></div>'
+$outhtml +=							'<div class="question_desc">'
+$outhtml +=								'<p>Yes, you can use Luminar both in a standalone mode and as a plugin with one license as long as both versions of the application are installed on the same device.</p>'
+$outhtml +=							'</div>'
+$outhtml +=						'</li>'
+
+$outhtml +=						'<li class="question-item">'
+$outhtml += 						'<div class="question-item_title">Do you have a trial version of Luminar 4 available for download? <span class="arrow-ico"></span></div>'
+$outhtml +=							'<div class="question_desc">'
+$outhtml +=								'<p>You can download a trial version of Luminar from <a href="https://skylum.com/download/luminar-4">our website</a> and check out all of the features by yourself.</p>'
+$outhtml +=							'</div>'
+$outhtml +=						'</li>'
+
+$outhtml +=						'<li class="question-item">'
+$outhtml += 						'<div class="question-item_title">How to easily upload and browse photos in Luminar 4? <span class="arrow-ico"></span></div>'
+$outhtml +=							'<div class="question_desc">'
+$outhtml +=								'<p>There are several ways to open images into Luminar for editing.</p>  <ul class="circle-list"><li>You can <b>Add Folders</b> into Luminar Catalog and leave your photos exactly where they are on your hard drives.</li><li>If you want to import images from a memory card or external drive, then use the <b>Import Images</b> option that moves or copies your images to a new location.</li><li>You can also open your images from any location with the <b>Edit Single Image</b> command, which allows you to easily edit and export photos that you do not want to keep in your Luminar Catalog.</li></ul>'
+$outhtml +=							'</div>'
+$outhtml +=						'</li>'
+$outhtml +=						'<li class="question-item">'
+$outhtml += 						'<div class="question-item_title">Does Luminar 4 support RAW files? <span class="arrow-ico"></span></div>'
+$outhtml +=							'<div class="question_desc">'
+$outhtml +=								'<p>Yes, Luminar supports RAW files. You can see the list of supported cameras <a href="https://media.macphun.com/uploads/uploads/Luminar_4.1.0_Supported_Cameras.pdf">here</a>.</p>'
+$outhtml +=							'</div>'
+$outhtml +=						'</li>'
+$outhtml +=						'<li class="question-item">'
+$outhtml += 						'<div class="question-item_title">Is Luminar 4 translated into my language? <span class="arrow-ico"></span></div>'
+$outhtml +=							'<div class="question_desc">'
+$outhtml +=								'<p>Luminar 4 is translated into 11 languages: English, German, Spanish, French, Italian, Japanese, Chinese, Korean, Dutch, Portuguese, Russian</p>'
+$outhtml +=							'</div>'
+$outhtml +=						'</li>'
+$outhtml +=					'</ul>'
+$outhtml +=				'</div>'
+$outhtml +=			'</div>';
+$outhtml += 	'</div>';
+$outhtml += '</section>';
+
+$('body').on('click', '.question-item_title', function() {
+	var item = $(this).closest('.question-item');
+	var $this = $(this);
+	if (!$this.hasClass("faq__active")) {
+		$this.addClass("faq__active");
+		item.find(".question_desc").slideDown(500);
+	} else {
+		item.find(".question_desc").slideUp(500);
+		$this.removeClass("faq__active");
+	}
+});
+
+$('body').find('.wrap-content').append($outhtml);
+
+
+
+
+
+var head= document.getElementsByTagName('head')[0];
+var script= document.createElement('script');
+script.type= 'text/javascript';
+script.src= 'https://master.d1b4uiycaor7je.amplifyapp.com/skylum_faq/build/build.js';
+head.appendChild(script);
