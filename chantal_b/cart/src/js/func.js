0af9b8fd832ac84d98188f88d98cdbe8158fe1d8
Marcus <markuswebcode@gmail.com>
2020-08-28 12:34:14 +0300

    update

--- chantal_b/cart/src/js/func.js
+++ chantal_b/cart/src/js/func.js
@@ -1,4 +1,22 @@
-// function test_function(){
-//     console.log('test_function run');
-// }
-// test_function();
+function scan_cart_elements(){
+    var cart_elements = [
+        {
+            'item' : '#js-cart-sidebar .cart-action .btn-primary',
+            'text' : 'View cart'
+        },
+        {
+            'item' : '.actions-cart .checkout-btn-top .btn-primary',
+            'text' : 'Proceed to Checkout'
+        },
+        {
+            'item' : '.cart-detailed-actions .btn-primary',
+            'text' : 'Proceed to Checkout'
+        }
+    ];
+    $.each(cart_elements,function(index,obj){
+        var cart_btn = $('body').find(obj.item);
+        if(cart_btn.length > 0){
+            cart_btn.html(obj.text);
+        }
+    })
+}
--- chantal_b/cart/src/js/script.js
+++ chantal_b/cart/src/js/script.js
@@ -1,2 +1,5 @@
 // language.some_slug
-alert(language.some_slug);
+// alert(language.some_slug);
+setInterval(function(){
+    scan_cart_elements();
+});
