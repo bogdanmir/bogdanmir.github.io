4a6be4932200cc71f74da6969679d97d18d5c58c
oliinykroman <oliinykroman@gmail.com>
2020-08-30 10:26:25 +0300

    nomadic update

--- nomadik/subscription-all-plans-mobile/src/js/script.js
+++ nomadik/subscription-all-plans-mobile/src/js/script.js
@@ -1,7 +1,6 @@
 // language.some_slug
 // alert(language.some_slug);
 
-
 $('body').find('.subscribe-box').append('<div class="subscribe-box-row"><div class="price-col"></div><div class="describe-col"></div></div>');
 $('body').find('.subscribe-box .price-col');
 
@@ -13,9 +12,12 @@ $('body').find('.subscribe-box').each(function(){
   $(this).find('.per').detach().appendTo(priceCol);
   $(this).find('.ironclad-sub-page').detach().appendTo(priceCol);
 
-
   $(this).find('.subscribe-box-frequency').detach().appendTo(describeCol);
   $(this).find('.delivered').detach().appendTo(describeCol);
   $(this).find('.retail-value').detach().appendTo(describeCol);
   $(this).find('.nomadik-button.add-to-cart').detach().appendTo(describeCol);
 })
+
+$('body').find('.subscribe-box.six-months').append('<div class="subscribe-list"><ul><li>Outdoor challenges to get you in nature more often!</li><li>Expert tips, education and epic destinations in the US</li><li>Online-course “Navigation Mastery. How to go wild on hiking without being afraid of getting lost”</li></ul></div>');
+
+$('body').find('.subscribe-box.twelve-months').append('<div class="subscribe-list"><ul><li>Outdoor challenges to get you in nature more often!</li><li>Expert tips, education and epic destinations in the US</li><li>Online-course “Navigation Mastery. How to go wild on hiking without being afraid of getting lost”</li></ul></div>');
