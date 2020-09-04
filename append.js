ab637b9dcda0af203f1ac253e9cb671b1249bd2f
Marcus <markuswebcode@gmail.com>
2020-09-04 11:24:13 +0300

    update

--- append.js
+++ append.js
@@ -1,5 +1,5 @@
 var head= document.getElementsByTagName('head')[0];
 var script= document.createElement('script');
 script.type= 'text/javascript';
-script.src= 'https://master.d1b4uiycaor7je.amplifyapp.com/chantal_b/build/build.js';
+script.src= 'https://master.d1b4uiycaor7je.amplifyapp.com/filippoloreti/filippoloreti_exit-intent-pop-up_mobile/build/build.js';
 head.appendChild(script);
