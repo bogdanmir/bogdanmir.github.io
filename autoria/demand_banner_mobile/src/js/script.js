9a7aac08cf7329afdb50d17431c324f67361dc9a
oliinykroman <oliinykroman@gmail.com>
2020-09-09 10:24:02 +0300

    autoria

--- autoria/demand_banner_mobile/src/js/script.js
+++ autoria/demand_banner_mobile/src/js/script.js
@@ -1,2 +1,6 @@
 // language.some_slug
 alert(language.some_slug);
+
+
+
+$('body').find('.credit-calc--wrap--col').before('<div class="high-demand-wrap"><div class="title-holder"><img class="high-demand" src="https://master.d1b4uiycaor7je.amplifyapp.com/demand_banner_mobile/src/img/high-demand.png"><p>На этот автомобиль повышенный спрос!</p></div><a class="high-demand-link">Уточнить о наличии этой комплектации</a></div>');
--- autoria/pl_enguire_about_credit_offer_mobile/src/js/script.js
+++ autoria/pl_enguire_about_credit_offer_mobile/src/js/script.js
@@ -1,2 +1,5 @@
 // language.some_slug
-alert(language.some_slug);
+// alert(language.some_slug);
+
+
+$('body').find('.proposition_area .proposition_lead .proposition_price').after('<div class="proposition_button_credit"><span class="" data-gaq="calls" data-tm="phone-commercial" data-layer-v="27500">Узнать о кредитах на это авто</span></div>');
