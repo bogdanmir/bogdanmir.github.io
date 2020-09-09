59f76215320e2fae96ea9c04ea0c75b265cb083f
oliinykroman <oliinykroman@gmail.com>
2020-09-09 17:28:26 +0300

    autoria

---
+++ autoria/pdp_sticky_button_mobile/src/js/script.js
@@ -0,0 +1,4 @@
+// language.some_slug
+// alert(language.some_slug);
+
+$('body').find('.seller-contact_m.fixbar').append('<div class="seller-item-credit '+language.credit_class+'"><a href="#" class="proposition_button_credit">'+language.credit_text+'</a></div>');
