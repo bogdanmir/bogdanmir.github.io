3c2d79299c89637919e630ef987224410d454d7e
oliinykroman <oliinykroman@gmail.com>
2020-09-17 12:06:33 +0300

    update moonmagic

--- moonmagic-test/desktop/pdp-row-remove-foursixty-banner/src/js/events.js
+++ moonmagic-test/desktop/pdp-row-remove-foursixty-banner/src/js/events.js
@@ -1,3 +1,23 @@
-// $(document).on('click','body',function (event) {
-//         console.log('body click');
-// });
+// Hotjar trigger to record video sessions
+try {
+(function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:1651990,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+window.hj = window.hj || function(){(hj.q = hj.q || []).push(arguments)};
+        hj('trigger', 'Experiment__remove_foursixty_banner');
+        console.log('h,o,t,j,a,r');
+}
+catch(e) {}
+
+// Activation of the alt variation
+window.dataLayer = window.dataLayer || [];
+dataLayer.push({
+        'event': 'event-to-ga',
+        'eventCategory': 'ROW, PDP: Remove FourSixty Banner',
+        'eventAction': 'loaded'
+});
