8737af3e7e20b3dc31787dbb4a4b94b2cdb1e6cd
oliinykroman <oliinykroman@gmail.com>
2020-09-18 12:53:48 +0300

    update dressa

--- dressa/mobile/pdp_exit_intent_pop_up/src/js/events.js
+++ dressa/mobile/pdp_exit_intent_pop_up/src/js/events.js
@@ -18,6 +18,23 @@
 // }
 // catch(e) {}
 
+try {
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:1914694,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+window.hj=window.hj||function(){(hj.q=hj.q||[]).push(arguments)};
+
+        hj('trigger', 'PDP_exit_intent_pop_up');
+}		
+catch(e) {
+	console.log('hotjar failed to load');
+}
+
 // Activation of the alt variation
 window.dataLayer = window.dataLayer || [];
 dataLayer.push({
