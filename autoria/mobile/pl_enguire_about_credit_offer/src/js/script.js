a67e61b5dead17afd0e0b47cfe79fa2c0504ef57
oliinykroman <oliinykroman@gmail.com>
2020-09-21 14:32:17 +0300

    update autoria

--- autoria/mobile/pl_enguire_about_credit_offer/src/js/script.js
+++ autoria/mobile/pl_enguire_about_credit_offer/src/js/script.js
@@ -2,5 +2,5 @@
 // alert(language.credit_text);
 
 
-$('body').find('.proposition_area .proposition_lead .proposition_price').after('<div class="proposition_button_credit '+language.credit_class+'"><span class="" data-gaq="calls" data-tm="phone-commercial" data-layer-v="27500">'+language.credit_text+'</span></div>');
+$('body').find('.proposition_area .proposition_lead .proposition_price').after('<div class="proposition_button_credit '+language.credit_class+'"><span class="" data-layer-v="27500">'+language.credit_text+'</span></div>');
 
