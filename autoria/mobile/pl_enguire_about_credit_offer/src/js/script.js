2d672fdd8dcf8d061e0eab90f3b5d6a354d7a83b
oliinykroman <oliinykroman@gmail.com>
2020-09-22 11:15:59 +0300

    update autoria

--- autoria/mobile/pl_enguire_about_credit_offer/src/js/script.js
+++ autoria/mobile/pl_enguire_about_credit_offer/src/js/script.js
@@ -2,5 +2,11 @@
 // alert(language.credit_text);
 
 
-$('body').find('.proposition_area .proposition_lead .proposition_price').after('<div class="proposition_button_credit '+language.credit_class+'"><span class="" data-layer-v="27500">'+language.credit_text+'</span></div>');
 
+
+
+setInterval(function() {
+	if( $('.proposition_button_credit').length == 0 ) {
+		$('body').find('.proposition_area .proposition_lead .proposition_price').after('<div class="proposition_button_credit '+language.credit_class+'"><span class="" data-layer-v="27500">'+language.credit_text+'</span></div>');
+	}
+})
