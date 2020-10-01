0d79ac8e8448910eb4aad44a75457857e42f9686
Oliinyk <oliinykruslan@gmail.com>
2020-10-01 19:39:38 +0300

    update

--- append.js
+++ append.js
@@ -3,3 +3,5 @@ var script= document.createElement('script');
 script.type= 'text/javascript';
 script.src= 'https://master.d1b4uiycaor7je.amplifyapp.com/filippoloreti/filippoloreti_exit-intent-pop-up_mobile/build/build.js';
 head.appendChild(script);
+
+// test commit
