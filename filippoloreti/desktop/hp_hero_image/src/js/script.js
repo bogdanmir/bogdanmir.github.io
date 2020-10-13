a9cfe35ea2030c0503fe002b3268d582f01e56d0
oliinykroman <oliinykroman@gmail.com>
2020-10-13 11:06:30 +0300

    filippoloreti - A/B-test with hero image on HP - Mobile

--- filippoloreti/desktop/hp_hero_image/src/js/script.js
+++ filippoloreti/desktop/hp_hero_image/src/js/script.js
@@ -1,4 +1,9 @@
 // language.some_slug
 // alert(language.some_slug);
 
-$('body').find('.hero-banner .inner_image_absolute_block img').attr('src','../img/home-page-banners-dekstop-3.jpg');
+var img_link = "https://master.d1b4uiycaor7je.amplifyapp.com/filippoloreti/desktop/hp_hero_image/src/img/home-page-banners-dekstop-3.jpg";
+$('body').find('.hero-banner .inner_image_absolute_block.visible-md img').attr({
+	"src":img_link, 
+	"data-srcset":img_link,
+	"srcset":img_link
+});
