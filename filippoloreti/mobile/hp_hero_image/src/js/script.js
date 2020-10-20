8979197bfccc4c281dc653ed2edac9547a1f6751
oliinykroman <oliinykroman@gmail.com>
2020-10-20 12:01:32 +0300

    filippoloreti mobile

--- filippoloreti/mobile/hp_hero_image/src/js/script.js
+++ filippoloreti/mobile/hp_hero_image/src/js/script.js
@@ -1,7 +1,7 @@
 // language.some_slug
 // alert(language.some_slug);
 
-var img_link = "https://master.d1b4uiycaor7je.amplifyapp.com/filippoloreti/mobile/hp_hero_image/src/img/home-page-banners-dekstop-3.jpg";
+var img_link = "https://master.d1b4uiycaor7je.amplifyapp.com/filippoloreti/mobile/hp_hero_image/src/img/home-page-banners-3.jpg";
 $('body').find('.hero-banner .inner_image_absolute_block.visible-sm img').attr({
 	"src":img_link, 
 	"data-srcset":img_link,
