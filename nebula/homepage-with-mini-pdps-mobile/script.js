47dd7b62be5c2fd0fd9c92cf5d2ffef4f7f490aa
htmldiz <mark.tanasevich@gmail.com>
2021-03-26 18:51:50 +0200

    update

--- nebula/homepage-with-mini-pdps-mobile/script.js
+++ nebula/homepage-with-mini-pdps-mobile/script.js
@@ -1544,6 +1544,7 @@ __addev('.plan-selection .form-check-input','change',function(e){
         addSubscriptionTo(_this);
     },10,_this);
 });
+
 var plust = __new('div');
 var minust = __new('div');
 __addclass(plust,'plust-qty');
