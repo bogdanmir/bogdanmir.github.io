622e7a699e3a3d06ee9bf50d2c018ca7daaa55df
htmldiz <mark.tanasevich@gmail.com>
2021-04-05 14:40:46 +0200

    fix

--- nebula/homepage-with-mini-pdps-mobile/script.js
+++ nebula/homepage-with-mini-pdps-mobile/script.js
@@ -1666,7 +1666,6 @@ function chanfge_page(){
     }
 }
 chanfge_page();
-
 if(document.querySelectorAll('.customers').length > 0){
     var link = document.createElement('link');link.rel = "stylesheet";link.href = "https://unpkg.com/swiper@6.5.0/swiper-bundle.min.css";document.querySelector('body').appendChild(link);
     var element = document.querySelector('.customers').cloneNode(true);
